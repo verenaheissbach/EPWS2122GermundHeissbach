434f3db4d826341920a916d5d24f76cc84db3f4d
annegermund <73778327+annegermund@users.noreply.github.com>
2021-12-13 12:40:02 +0100

    Aktuellen Stand des Codes eingefügt
    Enthält Temperatur- und Wasserstandsüberwachung

---
+++ Rapid Prototype/aquaponik.py
@@ -0,0 +1,163 @@
+from MCP3008 import MCP3008
+import os, time
+from datetime import datetime # für aktuelle Zeit
+import RPi.GPIO as GPIO
+from pushbullet import Pushbullet
+import glob
+
+import argparse
+
+from influxdb import InfluxDBClient
+from influxdb.client import InfluxDBClientError
+
+
+
+
+#### Grundeinstellungen
+
+## Pushbullet
+pb = Pushbullet("o.tYxGLZRlsrkViFCJDgUmjw2cYqhOSAId")
+print(pb.devices)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+## InfluxDB
+
+USER = 'admin'
+PASSWORD = 'aquaPonik21'
+DBNAME = 'aquaponik'
+
+host='localhost'
+port=8086
+    
+client = InfluxDBClient(host, port, USER, PASSWORD, DBNAME)
+
+
+SETTINGS = {
+    "WATER_GPIO": 25, #GPIO Number (BCM) for the Relay
+    "WATER_CHANNEL": 1, # of MCP3008
+    "WATER_MIN": 40, #Minimum water level
+    "WATER_MAX": 60, #Maximum water level
+    "WATERING_TIME": 1, #Watering time in sec
+    
+    "TEMP_MAX": 28, #max Watertemp in deg
+    "TEMP_MIN": 24, #min Watertemp in deg
+}
+
+adc = MCP3008()
+
+### TEMPERATUR
+
+## Modprobe Kommandos, um die Module für den Temperatursensor zu laden
+
+os.system("modprobe w1-gpio")
+os.system("modprobe w1-therm")
+
+## Variablen für den Pfad der Sensordaten
+
+base_dir = "/sys/bus/w1/devices/"
+device_folder = glob.glob(base_dir + "28*")[0]
+device_file = device_folder + "/w1_slave"
+
+## Funktionen
+
+def read_temp_raw():
+    f = open(device_file, "r")
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def getTemperature():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != "YES":
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find("t=")
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+    
+def temperatureControl():
+    t = getTemperature()
+    
+    data = []
+    data.append(
+        {
+            "measurement": "temperatur",
+            "tags": {
+                "sensor": "DS18B20"
+            },
+            "fields": {
+                "T": t
+            }
+        }
+    )
+
+    client.write_points(data)
+    
+    ## Benachrichtigung
+    
+    if t < SETTINGS["TEMP_MIN"]:
+        dev = pb.get_device("Galaxy Note 9")
+        push = dev.push_note("Warnung", "Die Temperatur ist zu niedrig! Bitte prüfen.")
+        print("Temperatur zu niedrig!  %.2f Grad Celsius" % (t))
+    
+    
+          
+## WASSERSTAND
+
+def getWaterLevel():
+    v = 0
+    for i in range(10):
+        v += (-0.0336 * float(adc.read(channel = SETTINGS["WATER_CHANNEL"])) + 157.608)
+    v /= 10.0
+    return v
+
+    
+
+def waterLevelControl(): # get water level data
+    adc = MCP3008()
+
+    v = getWaterLevel()
+    
+    data = []
+    data.append(
+        {
+            "measurement": "wasserstand",
+            "tags": {
+                "sensor": "Wasserstandssensor"
+            },
+            "fields": {
+                "pegel": v
+            }
+        }
+    )
+
+    client.write_points(data) 
+    
+    
+    if v < SETTINGS["WATER_MIN"]:
+        dev = pb.get_device("Galaxy Note 9")
+        push = dev.push_note("Warnung", "Der Wasserstand ist zu niedrig! Wasser wird nachgefüllt.")
+        print("Wasserpegel zu niedrig!  %.2f cm" % (v))
+        GPIO.setup(SETTINGS["WATER_GPIO"], GPIO.OUT, initial=GPIO.HIGH)
+    
+        while (True):
+            time.sleep(SETTINGS["WATERING_TIME"])
+            
+            value = getWaterLevel()
+            if value > (SETTINGS["WATER_MIN"] + 2): # Das Ventil schließt erst bei einem Wasserstand von 42 cm
+                break
+            
+            
+        GPIO.output(SETTINGS["WATER_GPIO"], GPIO.LOW)
+        
+         
+        print("Wasserpegel OK!  %.2f cm" % (value))
+         
+        push = dev.push_note("Entwarnung", "Befüllung abgeschlossen.")
+    
+waterLevelControl()
+temperatureControl()
