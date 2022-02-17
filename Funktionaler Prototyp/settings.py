9c7d62492f25de885ec2d4fc488b174d8de2b60d
annegermund <73778327+annegermund@users.noreply.github.com>
2022-02-17 12:53:10 +0100

    Bewässerungszeit entfernt
    Wert wird nicht mehr benötigt, da Relais so lange geöffnet wird, bis ein normaler Wert erreicht ist

--- Funktionaler Prototyp/settings.py
+++ Funktionaler Prototyp/settings.py
@@ -1,9 +1,8 @@
 settings = {
 	"WATER_GPIO": 25, 
 	"WATER_CHANNEL": 1, 
-	"WATER_MIN": 40, 
-	"WATER_MAX": 65, 
-	"WATERING_TIME": 1, 
-	"TEMP_MAX": 30, 
-	"TEMP_MIN": 35
+	"WATER_MIN": 55, 
+	"WATER_MAX": 55, 
+	"TEMP_MIN": 25, 
+	"TEMP_MAX": 35
 }
